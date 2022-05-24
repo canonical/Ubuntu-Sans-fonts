f1a0423bf79963fcab6033e998c533a9901a998e
Guido Ferreyra <guido@tipografia.com.ar>
2022-05-24 16:06:33 -0300

    Removed OFL edit from first run script

--- scripts/first-run.py
+++ scripts/first-run.py
@@ -107,15 +107,6 @@ readme = readme.replace(
 with open("README.md", "w") as fh:
     fh.write(readme)
 
-# Fix the OFL
-
-ofl = open("OFL.txt").read()
-ofl = ofl.replace(web_url(BASE_OWNER, BASE_REPONAME), web_url(owner, reponame))
-ofl = ofl.replace("My Font", reponame.title())
-ofl = ofl.replace("20**", str(datetime.date.today().year))
-with open("OFL.txt", "w") as fh:
-    fh.write(ofl)
-
 # Pin the dependencies
 print("Pinning dependencies")
 dependencies = subprocess.check_output(["pip", "freeze"])
