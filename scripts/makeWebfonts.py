392a0eaea2425ae45024efed08aad37acffef0d8
David Ross <6578861+djrrb@users.noreply.github.com>
2023-01-06 13:03:01 -0500

    add latin-minimal #51

--- scripts/makeWebfonts.py
+++ scripts/makeWebfonts.py
@@ -49,6 +49,8 @@ def subsetFont(fontPath, outputDir):
     UNICODERANGE_DATA = {
         "latin": "U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215",
         "latin-extended": "U+0100-024F, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2C60-2C7F, U+A720-A7FF",
+        "latin-minimal": "U+0000-00FF, U+0131, U+0152, U+0153, U+02BB, U+02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD"
+
         "cyrillic": "U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116",
         "cyrillic-extended": "U+0460-052F, U+20B4, U+2DE0-2DFF, U+A640-A69F",
         "greek": "U+0370-03FF",
