d814584420ea1cea480f4d916fb4530ba68f68ff
Guido Ferreyra <guido@tipografia.com.ar>
2023-05-30 15:27:42 -0300

    Build 0.869

--- scripts/hint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-ttfPath="../fonts/ttf"
-referenceFontPath="../fonts/ttf/Ubuntu-Regular.ttf"
-for file in $ttfPath/*; do 
-    ttfautohint $file ${file//.ttf/-hinted.ttf} -R $referenceFontPath
-    echo hinted $file
-done
